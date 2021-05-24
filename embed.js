bfa1290a2f2d2be3a2438b888f5b832637697922
Anthony Lee <me@anthonys.space>
2021-05-24 03:37:41 +0000

    Focus fixes

--- embed.js
+++ embed.js
@@ -136,7 +136,8 @@ function setDisplayMode(mode){
 	if (mode=="contrast"){
 		createCssRule("mode", "*", "color: rgb(0,0,0) !important;background: rgb(255,255,255) !important;");
 		createCssRule("mode", "input,button", "border: 2px solid rgb(0,0,0) !important;");
-		createCssRule("mode", "a", "color:rgb(69,130,240) !important;text-decoration: underline !important;");
+		createCssRule("mode", "a", "color:rgb(69,130,240) !important;font-weight: 700 !important;");
+		createCssRule("mode", "a:hover, a:focus", "border: 2px solid rgb(0,0,0) !important;padding:10px;background: rgb(255,255,255) !important;color:rgb(69,130,240) !important;font-weight: 700 !important;");
 	}
 }
 
