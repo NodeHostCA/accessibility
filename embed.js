b296fa8141137307b21994ed891d65d192350631
Anthony Lee <me@anthonys.space>
2021-07-24 03:13:07 +0000

    Fixes

--- embed.js
+++ embed.js
@@ -129,8 +129,8 @@ function setDisplayMode(mode){
 	clearCss("mode");
 	
 	if (mode=="invert"){
-		createCssRule("mode", "*:not(#accessibility-modl):empty", "filter: invert(1); !important;");
-		createCssRule("mode", "body", "background:rgb(255,255,255) !important;color:rgb(0,0,0) !important;");
+		createCssRule("mode", "*:not(#accessibility-modl)", "filter: invert(1); !important;");
+		createCssRule("mode", "*", "background:rgb(255,255,255) !important;color:rgb(0,0,0) !important;");
 		createCssRule("mode", "img:not(#accessibility-modl)", "filter: invert(1); !important;"); //Reverse flip of images
 	}
 	
